acff6b017b009bfb96747e468138b4c5bd07f80d
Ahmad <ahmadalkhateeb86@gmail.com>
2024-07-22 16:06:03 +0200

    Refactor scrape.py to use BeautifulSoup for web scraping and update restaurant URLs

--- scrape.py
+++ scrape.py
@@ -1,18 +1,52 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+# import requests
+# from bs4 import BeautifulSoup
 
-Options().add_argument('--headless')
-Options().add_argument('--no-sandbox')
-Options().add_argument('--disable-dev-shm-usage')
+# # URL of the page to scrape
+# url = 'https://www.restaurangspill.se/'
 
-# Assuming you have the Chrome WebDriver installed
-driver = webdriver.Chrome()
+# # Send an HTTP request to fetch the page content
+# response = requests.get(url)
+# response.raise_for_status()  # Ensure we notice bad responses
 
-driver.get('https://www.restaurangspill.se/')
+# # Parse the HTML content using BeautifulSoup
+# soup = BeautifulSoup(response.content, 'html.parser')
 
-# Find the element using XPath
-spill = driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[2]/div[1]/div[3]/div/div')
-driver.quit()
+# # Locate the specific div using its class and id
+# div = soup.find('div', class_='flex flex-col gap-4 md:gap-8')
 
-print(spill.text)
+# if div:
+#     print(div.get_text(separator='\n', strip=True))
+# else:
+#     print("Div not found")
+
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+# Load the list of restaurants from the JSON file
+with open('restaurants.json', 'r') as file:
+    restaurants = json.load(file)
+
+# Dictionary to store the scraped data
+scraped_menus = {}
+
+# Loop over each restaurant
+for restaurant in restaurants:
+    response = requests.get(restaurant['url'])
+    response.encoding = 'utf-8'  # Manually set encoding to UTF-8
+    html_content = response.text
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Dynamically execute the parser string from the JSON file
+    div = eval(restaurant['parser'])
+
+    if div:
+        print(div.get_text(separator='\n', strip=True))
+        scraped_menus[restaurant['name']] = div.get_text(separator='\n', strip=True)
+
+# Write the scraped menus to a new JSON file
+with open('scraped_menus.json', 'w', encoding='utf-8') as file:
+    json.dump(scraped_menus, file, indent=4, ensure_ascii=False)
+
+print("Scraping completed and data stored in 'scraped_menus.json'")
