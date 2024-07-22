30a804fe58e54905af47f75ad5b8223eb09ee4e1
Ahmad <ahmadalkhateeb86@gmail.com>
2024-07-22 14:27:37 +0200

    chore: Update scrape.py to use headless mode and disable sandbox and dev-shm-usage

--- scrape.py
+++ scrape.py
@@ -1,10 +1,10 @@
-# import requests
-# from bs4 import BeautifulSoup
-# import sqlite3
-# from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
+Options().add_argument('--headless')
+Options().add_argument('--no-sandbox')
+Options().add_argument('--disable-dev-shm-usage')
 
 # Assuming you have the Chrome WebDriver installed
 driver = webdriver.Chrome()
@@ -13,7 +13,6 @@ driver.get('https://www.restaurangspill.se/')
 
 # Find the element using XPath
 spill = driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[2]/div[1]/div[3]/div/div')
+driver.quit()
 
 print(spill.text)
-
-driver.quit()
