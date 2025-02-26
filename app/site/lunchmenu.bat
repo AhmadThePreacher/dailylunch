b18aebaaa8ca6f682d4d386d2ca4795802032cfe
AhmadThePreacher <ahmadshemliga@gmail.com>
2025-02-26 10:53:45 +0100

    Refactor lunchmenu.bat to streamline task execution and logging

--- app/site/lunchmenu.bat
+++ app/site/lunchmenu.bat
@@ -1,10 +1,7 @@
-@echo
-echo Starting Task >> C:\Projects\dailylunch\app\site\log.txt
+@echo off
 cd C:\Projects\dailylunch\app\site
 python scrape.py
 
 git add .
 git commit -m "Automated commit message"
 git push origin main
-
-echo Task Completed >> C:\Projects\dailylunch\app\site\log.txt
