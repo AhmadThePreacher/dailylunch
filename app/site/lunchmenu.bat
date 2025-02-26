93f8e7173d699407d33e0440b32388e66aa60afe
Ahmad Kadhem <ahmad.kadhem@nexergroup.com>
2025-02-26 10:50:36 +0100

    Automated commit message

--- app/site/lunchmenu.bat
+++ app/site/lunchmenu.bat
@@ -1,8 +1,10 @@
-@echo off
-cd c:\dailylunch\app\site
+@echo
+echo Starting Task >> C:\Projects\dailylunch\app\site\log.txt
+cd C:\Projects\dailylunch\app\site
 python scrape.py
 
 git add .
 git commit -m "Automated commit message"
 git push origin main
 
+echo Task Completed >> C:\Projects\dailylunch\app\site\log.txt
